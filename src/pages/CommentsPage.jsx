7bb55d5ee0411d01f9122d04365b2d87c81c0310
ShoneStew <macif50508@gmail.com>
2025-01-12 10:47:05 +0300

    Опять утренний коммит
    Что-то пофиксил, что именно - уже забыл

--- src/pages/CommentsPage.jsx
+++ src/pages/CommentsPage.jsx
@@ -5,36 +5,43 @@ import axios from "axios";
 
 function CommentsPage() {
     const { id } = useParams();
-    console.log("Айди:", id);
     const [nickname, setNickname] = useState('');
-    const [message, setMessage] = useState('');
-    const [yourMessage, setYourMessage] = useState(null);
-    const [topicMessages, setTopicMessages] = useState([]);
-    const [props, setProps] = useState([]);
+    const [comment, setComment] = useState('');
+    const [yourComment, setYourComment] = useState(null);
+    const [topicMessages, setTopicComments] = useState([]);
+    const [topicOne, setTopicOne] = useState([]);
 
-    // Загрузка данных с сервера
     useEffect(() => {
         async function fetchData() {
             try {
-                const topicMessagesTemp = await axios.get(`http://localhost:8080/api/v1/get_topic_info/${id}`);
-                const propsTemp = await axios.get(`http://localhost:8080/api/v1/get_topics_one/${id}`);
-                setTopicMessages(topicMessagesTemp.data);
-                setProps(propsTemp.data);
+                const topicMessages = await axios.get(`http://localhost:8080/api/v1/get_topic_info/${id}`);
+                const topicOne = await axios.get(`http://localhost:8080/api/v1/get_topics_one/${id}`);
+                setTopicComments(topicMessages.data);
+                setTopicOne(topicOne.data);
             } catch (error) {
                 console.error('Ошибка загрузки данных:', error);
-            }
-        }
+            };
+        };
         fetchData();
-    }, [id]); // Загрузка данных при изменении id
+    }, [id]);
 
-    const newMessage = async (e) => {
+    const newComment = async (e) => {
         e.preventDefault();
         let date = new Date();
-        // Можно добавить запрос для сохранения комментария на сервере
-        setYourMessage(
+        await axios.post("http://localhost:8080/api/v1/send_comment/", {
+            author: nickname,
+            text: comment,
+            date: {
+                day: date.getDate(),
+                month: date.getMonth() + 1,
+                year: date.getFullYear(),
+            },
+            topic_id: id,
+        });
+        setYourComment(
             <TopicComment 
                 author={nickname} 
-                text={message} 
+                text={comment} 
                 day={date.getDate()} 
                 month={date.getMonth() + 1} 
                 year={date.getFullYear()} 
@@ -46,12 +53,12 @@ function CommentsPage() {
         <main>
             <section className="topic-page">
                 <div className="topic-info">
-                    {props[0] && (
+                    {topicOne[0] && (
                         <>
-                            <h2>{props[0].title}</h2>
-                            <p className="description">{props[0].description}</p>
+                            <h2>{topicOne[0].title}</h2>
+                            <p className="description">{topicOne[0].description}</p>
                             <p className="info">
-                                Автор: {props[0].author} | Дата: {props[0].date.day}.{props[0].date.month}.{props[0].date.year} | Тема: {props[0].date.subject}
+                                Автор: {topicOne[0].author} | Дата: {topicOne[0].date.day}.{topicOne[0].date.month}.{topicOne[0].date.year} | Тема: {topicOne[0].subject}
                             </p>
                         </>
                     )}
@@ -74,10 +81,10 @@ function CommentsPage() {
                         ) : (
                             <p>Здесь пока ничего нету...</p>
                         )}
-                        {yourMessage}
+                        {yourComment}
                     </ul>
                     <h3>Добавить комментарий</h3>
-                    <form className="comment-form" onSubmit={newMessage}>
+                    <form className="comment-form" onSubmit={newComment}>
                         <label htmlFor="nickname">Никнейм:</label>
                         <input 
                             type="text" 
@@ -93,7 +100,7 @@ function CommentsPage() {
                             name="message" 
                             placeholder="Ваш комментарий" 
                             required 
-                            onChange={(e) => setMessage(e.target.value)} 
+                            onChange={(e) => setComment(e.target.value)} 
                         ></textarea>
                         <button type="submit">Отправить</button>
                     </form>
